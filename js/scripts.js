ed2a6100096cbc83f610a951a868b5b1e3e2b8a1
Matthew Craig <mgcraig78@gmail.com>
2020-06-14 22:27:16 -0700

    add new objects and methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,38 @@
+//business logic for addressbook ---
 function AddressBook(){
-  this.contacts = []:
+  this.contacts = [];
+  this.currentId = 0;
 }
+AddressBook.prototype.addContact = function(contact){
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+AddressBook.prototype.assignId = function(){
+  this.currentId += 1;
+  return this.currentId;
+}
+AddressBook.prototype.findContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+    if (this.contacts[i].id == id){
+      return this.contacts[i];
+    }
+  }
+  };
+  return false;
+}
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i=0; i<this.contacts.length; i++) {
+    if (this.contacts[i]){
+    if (this.contact[i].id == id){
+      delete this.contacts[i];
+      return true;
+    }
+  }
+  };
+  return false;
+}
+//business logic for contacts ---
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
